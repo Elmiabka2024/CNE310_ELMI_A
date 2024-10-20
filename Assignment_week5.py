153c7729c9f2775418d28c84582f97550a61259d
Elmiabka2024 <waraha01@gmail.com>
2024-10-20 16:19:21 -0700

    add assignment_week5

---
+++ Assignment_week5.py
@@ -0,0 +1,8 @@
+# This is my first assignment for week 5 github
+#Author: Elmi Aden , eaaden@student.rtc.edu
+#Creata Date: 10/20/2024
+school = 'Renton Technical College'
+program = 'BAS Computer Networking'
+course = 'Program Management in Networking'
+Professor = 'Zak'
+print(f' I am student at {school}, studying {program}, {course} by professor {Professor}')
